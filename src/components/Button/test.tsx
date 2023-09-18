d4cfc7259c3097c7d9692de59565989448ed80cb
gabyrodrigues <gabyrlima.99@gmail.com>
2023-09-18 19:23:13 -0300

    adding testing to Button sizes

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -1,12 +1,32 @@
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import Button from ".";
+import { renderWithTheme } from "@utils/tests/helpers";
 
 describe("<Button />", () => {
-  it("should render the heading", () => {
-    const { container } = render(<Button />);
+  it("should render the medium size by default", () => {
+    renderWithTheme(<Button>Buy now</Button>);
 
-    expect(screen.getByRole("heading", { name: /Button/i })).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: /Buy now/i })).toHaveStyle({
+      fontSize: "1.4rem",
+      height: "4rem",
+      padding: "0.8rem 3.2rem"
+    });
+  });
+  it("should render the small size", () => {
+    renderWithTheme(<Button size="small">Buy now</Button>);
 
-    expect(container.firstChild).toMatchSnapshot();
+    expect(screen.getByRole("button", { name: /Buy now/i })).toHaveStyle({
+      fontSize: "1.2rem",
+      height: "3rem"
+    });
+  });
+  it("should render the large size", () => {
+    renderWithTheme(<Button size="large">Buy now</Button>);
+
+    expect(screen.getByRole("button", { name: /Buy now/i })).toHaveStyle({
+      fontSize: "1.6rem",
+      height: "5rem",
+      padding: "0.8rem 4.8rem"
+    });
   });
 });
