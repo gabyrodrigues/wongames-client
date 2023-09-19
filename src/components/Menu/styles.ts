d307a8c1270c8e835fb8dd50af16d088f288a592
gabyrodrigues <gabyrlima.99@gmail.com>
2023-09-19 20:32:54 -0300

    Add animations to menu

--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -92,6 +92,7 @@ export const MenuFull = styled.nav<MenuFullProps>`
     position: absolute;
     right: 0;
     top: 0;
+    transition: opacity 0.3s ease-in-out;
 
     > svg {
       cursor: pointer;
@@ -116,6 +117,13 @@ export const MenuFull = styled.nav<MenuFullProps>`
       font-weight: ${theme.font.bold};
       font-size: ${theme.font.sizes.xlarge};
       margin-bottom: ${theme.spacings.small};
+      transform: ${$isOpen ? "translateY(0)" : "translateY(3rem)"};
+      transition: transform 0.3s ease-in-out;
+    }
+
+    ${RegisterBox} {
+      transform: ${$isOpen ? "translateY(0)" : "translateY(3rem)"};
+      transition: transform 0.3s ease-in-out;
     }
   `}
 `;
