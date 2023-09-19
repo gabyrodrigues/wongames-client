94f79b509d4d073d73960d0379be721015d46d02
gabyrodrigues <gabyrlima.99@gmail.com>
2023-09-19 19:11:51 -0300

    start developing Menu mobile

---
+++ src/components/Menu/styles.ts
@@ -0,0 +1,40 @@
+import styled, { css } from "styled-components";
+import media from "styled-media-query";
+
+export const Wrapper = styled.menu`
+  ${({ theme }) => css`
+    align-items: center;
+    display: flex;
+    padding: ${theme.spacings.small} 0;
+    position: relative;
+  `}
+`;
+
+export const LogoWrapper = styled.div`
+  ${media.lessThan("medium")`
+    left: 50%;
+    position: absolute;
+    transform: translateX(-50%);
+  `}
+`;
+
+export const IconWrapper = styled.div`
+  ${({ theme }) => css`
+    color: ${theme.colors.white};
+    cursor: pointer;
+    height: 2.4rem;
+    width: 2.4rem;
+  `}
+`;
+
+export const MenuGroup = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    flex-grow: 1;
+    justify-content: flex-end;
+
+    > div {
+      margin-left: ${theme.spacings.xsmall};
+    }
+  `}
+`;
