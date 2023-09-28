90236022850f607837884e9a1f2fbe5326bdeb36
gabyrodrigues <gabyrlima.99@gmail.com>
2023-09-28 19:51:26 -0300

    Add tests to GameCard promotionalPrice

--- src/components/GameCard/test.tsx
+++ src/components/GameCard/test.tsx
@@ -1,6 +1,7 @@
 import { screen } from "@testing-library/react";
 import { renderWithTheme } from "@utils/tests/helpers";
 import GameCard from ".";
+import theme from "@styles/theme";
 
 const props = {
   title: "Population Zero",
@@ -17,5 +18,32 @@ describe("<GameCard />", () => {
     expect(screen.getByRole("heading", { name: props.developer })).toBeInTheDocument();
     expect(screen.getByRole("img", { name: props.title })).toHaveAttribute("src", props.img);
     expect(screen.getByLabelText(/add to wishlist/i)).toBeInTheDocument();
+  });
+  it("should render price in label", () => {
+    renderWithTheme(<GameCard {...props} />);
+
+    expect(screen.getByText(props.price)).not.toHaveStyle({
+      color: "#8F8F8F",
+      textDecoration: "line-through"
+    });
+    expect(screen.getByText(props.price)).toHaveStyle({
+      backgroundColor: theme.colors.secondary
+    });
+  });
+  it("should render a line-through in price when promotional", () => {
+    renderWithTheme(
+      <GameCard
+        {...props}
+        promotionalPrice="R$ 200,00"
+      />
+    );
+
+    expect(screen.getByText(props.price)).toHaveStyle({
+      color: "#8F8F8F",
+      textDecoration: "line-through"
+    });
+    expect(screen.getByText("R$ 200,00")).not.toHaveStyle({
+      textDecoration: "line-through"
+    });
   });
 });
