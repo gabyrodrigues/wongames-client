f45da3985724c0d3b7409a2ff8780b65c19ae488
gabyrodrigues <gabyrlima.99@gmail.com>
2023-11-08 20:24:47 -0300

    Add Auth template tests

--- src/app/auth/test.tsx
+++ src/app/auth/test.tsx
@@ -1,12 +1,33 @@
-import { render } from "@testing-library/react";
+import { screen } from "@testing-library/react";
+import { renderWithTheme } from "@utils/tests/helpers";
 import AuthTemplate from "./template";
 
 describe("<Auth />", () => {
   it("should render all components and children", () => {
-    render(
+    renderWithTheme(
       <AuthTemplate>
         <input type="text" />
       </AuthTemplate>
     );
+
+    // verifiquem se tem 2 logos
+    expect(screen.getByRole("img", { name: /won games/i })).toBeInTheDocument();
+
+    // verifica se tem o heading principal do banner
+    expect(
+      screen.getByRole("heading", {
+        name: /All your favorite games in one place/i
+      })
+    ).toBeInTheDocument();
+
+    // verifica se tem o subtitle
+    expect(
+      screen.getByRole("heading", {
+        name: /won is the best and most complete gaming platform/i
+      })
+    ).toBeInTheDocument();
+
+    // verifica se o children tá sendo renderizado
+    expect(screen.getByRole("textbox")).toBeInTheDocument();
   });
 });
