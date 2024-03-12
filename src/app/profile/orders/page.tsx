f865e1ca2e69dbbdb4851bb992557c0e311d5962
gabyrodrigues <gabyrlima.99@gmail.com>
2024-03-12 19:41:39 -0300

    /profile/orders page

--- src/app/profile/orders/page.tsx
+++ src/app/profile/orders/page.tsx
@@ -1,11 +1,35 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 import Profile from "@templates/Profile";
+import itemsMock from "@components/OrdersList/mock";
+import OrdersList, { OrdersListProps } from "@components/OrdersList";
+import { GameItemProps } from "@components/GameItem";
 
 export default function ProfileOrdersPage() {
+  const [ordersData, setOrdersData] = useState<OrdersListProps>({
+    items: [] as GameItemProps[]
+  });
+
+  async function fetchProfileInfo() {
+    const ordersData = await getOrdersData();
+    setOrdersData(ordersData);
+  }
+
+  useEffect(() => {
+    fetchProfileInfo();
+  }, []);
+
   return (
     <Profile>
-      <h1>orders</h1>
+      <OrdersList items={ordersData.items} />
     </Profile>
   );
+}
+
+async function getOrdersData() {
+  return {
+    items: itemsMock
+  };
 }
